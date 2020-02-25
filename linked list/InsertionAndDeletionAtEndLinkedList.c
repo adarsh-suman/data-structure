204b3f82d92dd3923815b705029677645fcba8ef
adarsh-suman <61372794+adarsh-suman@users.noreply.github.com>
2020-02-25 16:27:19 +0530

    Update InsertionAndDeletionAtEndLinkedList.c

--- linked list/InsertionAndDeletionAtEndLinkedList.c
+++ linked list/InsertionAndDeletionAtEndLinkedList.c
@@ -1,1 +1,75 @@
+#include<stdio.h>
 
+struct node
+{
+    int data;
+    struct node *next;
+}
+
+*head;
+
+void print()
+{
+    if(head==NULL) 
+    {
+        printf("no element");
+        return;
+    }
+    struct node*temp=head;
+    while (temp!=NULL)
+    {
+        printf("%d\n",temp->data);
+        temp=temp->next;
+    }
+}
+
+void InsertAtEnd(int data)
+{
+    struct node* A=(struct node*) malloc(sizeof(struct node));
+    A->data=data;
+    A->next=NULL;
+    struct node *temp=head;
+    if (temp==NULL)
+    {
+        head=A;
+        return;
+    }
+    while (temp->next!=NULL)
+    {
+        temp=temp->next;
+    } 
+    temp->next=A;
+}
+
+void DeleteAtEnd()
+{
+    struct node *temp=head;
+    
+    if(head == NULL)
+        return;
+    struct node *B;
+    while(temp->next!=NULL && temp->next->next!=NULL)
+        temp=temp->next;
+    
+    if(temp->next == NULL)
+    {    
+        free(head);
+        head=NULL;
+        return;
+    }
+    B=temp->next;
+    temp->next=NULL;
+    free(B);
+    
+}
+
+
+void main()
+{
+    head=NULL;
+    InsertAtEnd(20);
+    InsertAtEnd(30);
+    print();
+    DeleteAtEnd();
+    print();
+}
